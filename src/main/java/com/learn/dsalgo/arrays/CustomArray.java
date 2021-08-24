4b9298928bbf57ac76fcb85f49587cd0574bdcf6
Mohit.Singh <Mohit.Singh@maersk.com>
2021-08-24 21:20:52 +0530

    initial commit

---
+++ src/main/java/com/learn/dsalgo/arrays/CustomArray.java
@@ -0,0 +1,15 @@
+package com.learn.dsalgo.arrays;
+
+public class CustomArray<T> {
+
+    public int addElement(T t, int index){
+        return 0;
+    }
+
+
+    public int deleteElement(int index){
+        return index;
+    }
+    
+    
+}
